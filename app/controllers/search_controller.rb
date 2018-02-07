5e8223a14f7c27233afde59cff7cc5904c7b724b
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-07 10:15:07 -0700

    trying to get json response convert into Stations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,10 +6,24 @@ class SearchController < ApplicationController
       faraday.headers['X-API-Key'] = ENV["ALTFUEL_API_KEY"]
       faraday.adapter Faraday.default_adapter
     end
-    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?#{params[:location]}")
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=80203")
 
-    @search = JSON.parse(response.body, symbolize_names: true)[:results].map do |station|
+    @search = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+    [:fuel_stations].map do |station|
       Station.new(station)
     end
+  end
+end
+
+class Station
+  attr_reader :name, :address, :fuel_type, :distance, :access_times
+
+  def initialize(station = {})
+    @name = station[:station_name]
+    @address = station[:street_address]
+    @fuel_type = station[:fuel_type_code]
+    @distance = station[:distance]
+    @access_times = station[:access_days_time]
   end
 end
