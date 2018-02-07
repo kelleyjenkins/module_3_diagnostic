54a9b1bb3703aab748c9df175fe8a1a1c6962af3
Kelley Jenkins <ckjenkins3@gmail.com>
2018-02-07 09:58:32 -0700

    Working out postman response .. needed root domain

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,15 @@
 class SearchController < ApplicationController
   def index
-    @search = StationSearch.new(params[:zip_code])
+    # @search = StationSearch.new(params[:zip_code])
 
-    conn = Faraday.new(url: "https://api.alt-fuel-stations/v1/nearest") do |faraday|
+    conn = Faraday.new(url: "https://developer.nrel.gov/") do |faraday|
       faraday.headers['X-API-Key'] = ENV["ALTFUEL_API_KEY"]
       faraday.adapter Faraday.default_adapter
+    end
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?#{params[:location]}")
 
-
+    @search = JSON.parse(response.body, symbolize_names: true)[:results].map do |station|
+      Station.new(station)
+    end
   end
 end
